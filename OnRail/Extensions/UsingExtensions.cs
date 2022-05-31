bafdb5c5cac2cca4ee160d18c355a7e3bc578afc
Hamid Molareza <kitm.hamid@gmail.com>
2022-05-31 12:11:12 +0430

    Completes UsingExtensions

--- OnRail/Extensions/UsingExtensions.cs
+++ OnRail/Extensions/UsingExtensions.cs
@@ -12,10 +12,45 @@ public static class UsingExtensions {
             }
         });
 
+    public static Result Using<T>(
+        this T obj,
+        Func<T, Result> function) where T : IDisposable =>
+        TryExtensions.Try(() => {
+            using (obj) {
+                return function(obj);
+            }
+        });
+
+    public static Result<TResult> Using<TSource, TResult>(
+        this TSource obj,
+        Func<TSource, Result<TResult>> function) where TSource : IDisposable =>
+        TryExtensions.Try(() => {
+            using (obj) {
+                return function(obj);
+            }
+        });
 
     public static Result<TResult> Using<TSource, TResult>(
         this TSource obj,
         Func<TResult> function) where TSource : IDisposable =>
+        TryExtensions.Try(() => {
+            using (obj) {
+                return function();
+            }
+        });
+
+    public static Result Using<TSource>(
+        this TSource obj,
+        Func<Result> function) where TSource : IDisposable =>
+        TryExtensions.Try(() => {
+            using (obj) {
+                return function();
+            }
+        });
+
+    public static Result<TResult> Using<TSource, TResult>(
+        this TSource obj,
+        Func<Result<TResult>> function) where TSource : IDisposable =>
         TryExtensions.Try(() => {
             using (obj) {
                 return function();
@@ -44,50 +79,72 @@ public static class UsingExtensions {
 
     #region UsingAsync
 
-    public static Task<TResult> UsingAsync<TSource, TResult>(
+    public static Task<Result<TResult>> UsingAsync<TSource, TResult>(
         this TSource obj,
-        Func<TSource, Task<TResult>> function) where TSource : IDisposable {
-        using (obj) {
-            return function(obj);
-        }
-    }
+        Func<TSource, Task<TResult>> function) where TSource : IDisposable =>
+        TryExtensions.TryAsync(async () => {
+            using (obj) {
+                return await function(obj);
+            }
+        });
+
+    public static Task<Result> UsingAsync<TSource>(
+        this TSource obj,
+        Func<Task<Result>> function) where TSource : IDisposable =>
+        TryExtensions.TryAsync(async () => {
+            using (obj) {
+                return await function();
+            }
+        });
+
+    public static Task<Result> UsingAsync<TSource>(
+        this TSource obj,
+        Func<TSource, Task<Result>> function) where TSource : IDisposable =>
+        TryExtensions.TryAsync(async () => {
+            using (obj) {
+                return await function(obj);
+            }
+        });
 
     public static Task<Result<TResult>> UsingAsync<TSource, TResult>(
         this TSource obj,
-        Func<TSource, Task<Result<TResult>>> function) where TSource : IDisposable {
-        using (obj) {
-            return function(obj);
-        }
-    }
-
-    public static Task<TResult> UsingAsync<TSource, TResult>(
-        this TSource obj,
-        Func<Task<TResult>> function) where TSource : IDisposable {
-        using (obj) {
-            return function();
-        }
-    }
+        Func<TSource, Task<Result<TResult>>> function) where TSource : IDisposable =>
+        TryExtensions.TryAsync(async () => {
+            using (obj) {
+                return await function(obj);
+            }
+        });
 
     public static Task<Result<TResult>> UsingAsync<TSource, TResult>(
         this TSource obj,
-        Func<Task<Result<TResult>>> function) where TSource : IDisposable {
-        using (obj) {
-            return function();
-        }
-    }
+        Func<Task<TResult>> function) where TSource : IDisposable =>
+        TryExtensions.TryAsync(async () => {
+            using (obj) {
+                return await function();
+            }
+        });
+
+    public static Task<Result<TResult>> UsingAsync<TSource, TResult>(
+        this TSource obj,
+        Func<Task<Result<TResult>>> function) where TSource : IDisposable =>
+        TryExtensions.TryAsync(async () => {
+            using (obj) {
+                return await function();
+            }
+        });
 
     #endregion
 
     #region TeeUsing
 
-    public static T TeeUsing<T>(
-        this T obj,
-        Action<T> action) where T : IDisposable =>
-        obj.Tee(() => obj.Using(action));
-
     public static TSource TeeUsing<TSource, TResult>(
         this TSource obj,
         Func<TSource, TResult> function) where TSource : IDisposable =>
+        obj.Tee(() => obj.Using(function));
+
+    public static TSource TeeUsing<TSource, TResult>(
+        this TSource obj,
+        Func<TResult> function) where TSource : IDisposable =>
         obj.Tee(() => obj.Using(function));
 
     #endregion
--- OnRailTest/Extensions/UsingExtensionsTest.cs
+++ OnRailTest/Extensions/UsingExtensionsTest.cs
@@ -1,3 +1,4 @@
+using OnRail;
 using OnRail.Extensions;
 using OnRail.ResultDetails.Errors;
 
@@ -31,7 +32,8 @@ public class UsingExtensionsTest {
         var disposableObj = new Disposable();
         Assert.False(disposableObj.IsDisposed);
 
-        var result = disposableObj.Using(() => throw new Exception());
+        void Action() => throw new Exception();
+        var result = disposableObj.Using(Action);
 
         Assert.True(disposableObj.IsDisposed);
         Assert.False(result.IsSuccess);
@@ -54,7 +56,8 @@ public class UsingExtensionsTest {
         var disposableObj = new Disposable();
         Assert.False(disposableObj.IsDisposed);
 
-        var result = disposableObj.Using(obj => throw new Exception());
+        void Action(Disposable obj) => throw new Exception();
+        var result = disposableObj.Using(Action);
 
         Assert.True(disposableObj.IsDisposed);
         Assert.False(result.IsSuccess);
@@ -92,6 +95,36 @@ public class UsingExtensionsTest {
     }
 
     [Fact]
+    public void Using_SuccessfulFunctionWithInputAndResultValueOutput_DisposeObject() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = disposableObj.Using(obj => {
+            Assert.False(obj.IsDisposed);
+            return Result<string>.Ok("result");
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.True(result.IsSuccess);
+        Assert.Equal("result", result.Value);
+    }
+
+    [Fact]
+    public void Using_FailFunctionWithInputAndResultValueOutput_DisposeAndReturnExceptionError() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = disposableObj.Using(obj => {
+            Assert.False(obj.IsDisposed);
+            return Result<string>.Fail();
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.False(result.IsSuccess);
+    }
+
+
+    [Fact]
     public void Using_SuccessfulFunctionWithOutput_DisposeObject() {
         var disposableObj = new Disposable();
         Assert.False(disposableObj.IsDisposed);
@@ -121,11 +154,326 @@ public class UsingExtensionsTest {
         Assert.False(result.IsSuccess);
     }
 
+    [Fact]
+    public void Using_SuccessfulFunctionWithResultValueOutput_DisposeObject() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = disposableObj.Using(() => {
+            Assert.False(disposableObj.IsDisposed);
+            return Result<string>.Ok("result");
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.True(result.IsSuccess);
+        Assert.Equal("result", result.Value);
+    }
+
+    [Fact]
+    public void Using_FailFunctionWithResultValueOutput_DisposeAndReturnExceptionError() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = disposableObj.Using(() => {
+            Assert.False(disposableObj.IsDisposed);
+            return Result<string>.Fail();
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.False(result.IsSuccess);
+    }
+
+    [Fact]
+    public void Using_SuccessfulFunctionWithResultOutput_DisposeObject() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = disposableObj.Using(() => {
+            Assert.False(disposableObj.IsDisposed);
+            return Result.Ok();
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public void Using_FailFunctionWithResultOutput_DisposeAndReturnExceptionError() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = disposableObj.Using(() => {
+            Assert.False(disposableObj.IsDisposed);
+            return Result.Fail();
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.False(result.IsSuccess);
+    }
+
+    [Fact]
+    public void Using_SuccessfulFunctionWithInputAndResultOutput_DisposeObject() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = disposableObj.Using(obj => {
+            Assert.False(disposableObj.IsDisposed);
+            return Result.Ok();
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public void Using_FailFunctionWithInputAndResultOutput_DisposeAndReturnExceptionError() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = disposableObj.Using(obj => {
+            Assert.False(disposableObj.IsDisposed);
+            return Result.Fail();
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.False(result.IsSuccess);
+    }
+
     #endregion
 
     #region UsingAsync
 
-    
+    [Fact]
+    public async Task UsingAsync_SuccessfulFunctionWithInputOutput_DisposeObject() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = await disposableObj.UsingAsync(obj => {
+            Assert.False(obj.IsDisposed);
+            return Task.FromResult("result");
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.True(result.IsSuccess);
+        Assert.Equal("result", result.Value);
+    }
+
+    [Fact]
+    public async Task UsingAsync_FailFunctionWithInputOutput_DisposeAndReturnExceptionError() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = await disposableObj.UsingAsync(obj => {
+            Assert.False(obj.IsDisposed);
+            throw new Exception();
+            return Task.FromResult("result");
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.False(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task UsingAsync_SuccessfulFunctionWithInputAndResultValueOutput_DisposeObject() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = await disposableObj.UsingAsync(obj => {
+            Assert.False(obj.IsDisposed);
+            return Task.FromResult(Result<string>.Ok("result"));
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.True(result.IsSuccess);
+        Assert.Equal("result", result.Value);
+    }
+
+    [Fact]
+    public async Task UsingAsync_FailFunctionWithInputAndResultValueOutput_DisposeAndReturnExceptionError() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = await disposableObj.UsingAsync(obj => {
+            Assert.False(obj.IsDisposed);
+            return Task.FromResult(Result<string>.Fail());
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.False(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task UsingAsync_SuccessfulFunctionWithResultValueOutput_DisposeObject() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = await disposableObj.UsingAsync(() => {
+            Assert.False(disposableObj.IsDisposed);
+            return Task.FromResult(Result<string>.Ok("result"));
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.True(result.IsSuccess);
+        Assert.Equal("result", result.Value);
+    }
+
+    [Fact]
+    public async Task UsingAsync_FailFunctionWithResultValueOutput_DisposeAndReturnExceptionError() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = await disposableObj.UsingAsync(() => {
+            Assert.False(disposableObj.IsDisposed);
+            return Task.FromResult(Result<string>.Fail());
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.False(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task UsingAsync_SuccessfulFunctionWithResultOutput_DisposeObject() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = await disposableObj.UsingAsync(() => {
+            Assert.False(disposableObj.IsDisposed);
+            return Task.FromResult(Result.Ok());
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task UsingAsync_FailFunctionWithResultOutput_DisposeAndReturnExceptionError() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = await disposableObj.UsingAsync(() => {
+            Assert.False(disposableObj.IsDisposed);
+            return Task.FromResult(Result.Fail());
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.False(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task UsingAsync_SuccessfulFunctionWithInputAndResultOutput_DisposeObject() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = await disposableObj.UsingAsync(obj => {
+            Assert.False(obj.IsDisposed);
+            return Task.FromResult(Result.Ok());
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task UsingAsync_FailFunctionWithInputAndResultOutput_DisposeAndReturnExceptionError() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = await disposableObj.UsingAsync(obj => {
+            Assert.False(obj.IsDisposed);
+            return Task.FromResult(Result.Fail());
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.False(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task UsingAsync_SuccessfulFunctionWithOutput_DisposeObject() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = await disposableObj.UsingAsync(() => {
+            Assert.False(disposableObj.IsDisposed);
+            return Task.FromResult("result");
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.True(result.IsSuccess);
+        Assert.Equal("result", result.Value);
+    }
+
+    [Fact]
+    public async Task UsingAsync_FailFunctionWithOutput_DisposeAndReturnExceptionError() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        var result = await disposableObj.UsingAsync(() => {
+            Assert.False(disposableObj.IsDisposed);
+            throw new Exception();
+            return Task.FromResult("result");
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+        Assert.False(result.IsSuccess);
+    }
+
+    #endregion
+
+    #region TeeUsing
+
+    [Fact]
+    public void TeeUsing_SuccessfulFunctionWithInputOutput_DisposeObject() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        disposableObj.TeeUsing(obj => {
+            Assert.False(obj.IsDisposed);
+            return "result";
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+    }
+
+    [Fact]
+    public void TeeUsing_FailFunctionWithInputOutput_DisposeAndReturnExceptionError() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        disposableObj.TeeUsing(obj => {
+            Assert.False(obj.IsDisposed);
+            throw new Exception();
+            return "result";
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+    }
+
+    [Fact]
+    public void TeeUsing_SuccessfulFunctionWithOutput_DisposeObject() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        disposableObj.TeeUsing(() => {
+            Assert.False(disposableObj.IsDisposed);
+            return "result";
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+    }
+
+    [Fact]
+    public void TeeUsing_FailFunctionWithOutput_DisposeAndReturnExceptionError() {
+        var disposableObj = new Disposable();
+        Assert.False(disposableObj.IsDisposed);
+
+        disposableObj.TeeUsing(() => {
+            Assert.False(disposableObj.IsDisposed);
+            throw new Exception();
+            return "result";
+        });
+
+        Assert.True(disposableObj.IsDisposed);
+    }
 
     #endregion
 }
