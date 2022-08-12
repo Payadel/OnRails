b8a79b000b5a614ad518af79b2c67ce1b3716522
Hamid Molareza <kitm.hamid@gmail.com>
2022-08-12 10:12:27 +0430

    Updates ThrowExceptionExtensions methods.

--- OnRail/Extensions/ThrowException/ThrowExceptionExtensions.OnFail.cs
+++ OnRail/Extensions/ThrowException/ThrowExceptionExtensions.OnFail.cs
@@ -2,22 +2,10 @@ namespace OnRail.Extensions.ThrowException;
 
 public static partial class ThrowExceptionExtensions {
     public static Result<T> ThrowExceptionOnFail<T>(
-        this Result<T> @this,
-        Exception? exception = null) => @this
-        .OnFail(result => {
-            if (exception is not null)
-                throw exception;
-
-            result.Detail.ThrowException();
-        });
+        this Result<T> @this) => @this
+        .OnFail(result => result.Detail.ThrowException());
 
     public static Result ThrowExceptionOnFail(
-        this Result @this,
-        Exception? exception = null) => @this
-        .OnFail(result => {
-            if (exception is not null)
-                throw exception;
-
-            result.Detail.ThrowException();
-        });
+        this Result @this) => @this
+        .OnFail(result => result.Detail.ThrowException());
 }
