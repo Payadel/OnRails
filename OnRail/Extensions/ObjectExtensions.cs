c88a9c91ea26186ff86b6b2d8428b5af5379045a
Hamid Molareza <kitm.hamid@gmail.com>
2022-08-15 15:32:08 +0430

    Updates ObjectExtensions

--- OnRail/Extensions/ObjectExtensions.cs
+++ OnRail/Extensions/ObjectExtensions.cs
@@ -1,34 +1,31 @@
+using OnRail.Extensions.OnFail;
+using OnRail.Extensions.OnSuccess;
+using OnRail.Extensions.Try;
+using OnRail.ResultDetails;
+
 namespace OnRail.Extensions;
 
 public static class ObjectExtensions {
-    // public static Result<T> IsNotNull<T>(
-    //     this object? @this,
-    //     ErrorDetail? errorDetail = null,
-    //     bool showDefaultMessageToUser = true) =>
-    //     @this.FailWhen(@this is null, errorDetail ?? new ErrorDetail(
-    //             StatusCodes.Status400BadRequest, title: "NullError",
-    //             message: "Object is null.", showDefaultMessageToUser: showDefaultMessageToUser))
-    //         .MapResult((T) @this!);
-    //
-    // public static Result<TResult> As<TResult>(
-    //     this object @this,
-    //     ErrorDetail? errorDetail = null,
-    //     bool showDefaultMessageToUser = true) =>
-    //     TryExtensions.Try(() => Convert.ChangeType(@this, typeof(TResult)))
-    //         .OnSuccess(obj => obj.IsNotNull<TResult>())
-    //         .OnFail(() =>
-    //             Result<TResult>.Fail(errorDetail ??
-    //                                  new ErrorDetail(StatusCodes.Status400BadRequest,
-    //                                      message:
-    //                                      $"({@this} - Type of ({@this.GetType()})) is not {typeof(TResult)}",
-    //                                      showDefaultMessageToUser: showDefaultMessageToUser)));
-    //
-    // public static Result AreNull(
-    //     this IEnumerable<object?> @this,
-    //     bool showDefaultMessageToUser = true) =>
-    //     @this.ForEachUntilIsSuccess(obj =>
-    //         FailExtensions.FailWhen(obj != null, new ErrorDetail(
-    //             StatusCodes.Status400BadRequest, message: $"{obj} is not null.",
-    //             showDefaultMessageToUser: showDefaultMessageToUser))
-    //     );
+    public static Result<T> IsNotNull<T>(
+        this object? @this,
+        ErrorDetail? errorDetail = null) =>
+        @this.FailWhen(@this is null, errorDetail ?? new ErrorDetail(
+                title: "NullError", message: "Object is null."))
+            .MapResult((T) @this!);
+
+    public static Result<TResult> As<TResult>(
+        this object @this,
+        ErrorDetail? errorDetail = null) =>
+        TryExtensions.Try(() => Convert.ChangeType(@this, typeof(TResult)))
+            .OnSuccess(obj => obj.IsNotNull<TResult>())
+            .OnFail(() =>
+                Result<TResult>.Fail(errorDetail ?? new ErrorDetail(
+                    message: $"({@this} - Type of ({@this.GetType()})) is not {typeof(TResult)}")));
+
+    public static Result AreNull(
+        this IEnumerable<object?> @this) =>
+        @this.ForEachUntilIsSuccess(obj =>
+            FailExtensions.FailWhen(obj != null, new ErrorDetail(
+                message: $"{obj} is not null.")
+            ));
 }
