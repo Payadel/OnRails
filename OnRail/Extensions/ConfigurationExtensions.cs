b89196393ad5eb4e39f8f8e43ffca6db3fff1139
Hamid Molareza <kitm.hamid@gmail.com>
2022-08-15 19:15:27 +0430

    Updates ConfigurationExtensions

--- OnRail/Extensions/ConfigurationExtensions.cs
+++ OnRail/Extensions/ConfigurationExtensions.cs
@@ -1,21 +1,27 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnRail.Extensions.Map;
+using OnRail.Extensions.OnSuccess;
+using OnRail.Extensions.Try;
+
 namespace OnRail.Extensions;
 
-// public static class ConfigurationExtensions {
-//     public static T GetConfig<T>(
-//         this IConfiguration @this) =>
-//         @this.GetSection(typeof(T).Name)
-//             .Get<T>();
-//
-//     public static Result<IConfiguration> AddConfig<T>(
-//         this IConfiguration @this,
-//         IServiceCollection services) where T : class =>
-//         @this
-//             .GetConfig<T>()
-//             .TryTee(services.AddSingleton!)
-//             .MapResult(@this);
-//
-//     public static Result<IConfiguration> AddConfig<T>(
-//         this Result<IConfiguration> @this,
-//         IServiceCollection services) where T : class => @this
-//         .OnSuccess(configuration => configuration.AddConfig<T>(services));
-// }
+public static class ConfigurationExtensions {
+    public static Result<T> GetConfig<T>(
+        this IConfiguration @this) =>
+        TryExtensions.Try(() => @this.GetSection(typeof(T).Name)
+            .Get<T>());
+
+    public static Result<IConfiguration> AddConfig<T>(
+        this IConfiguration @this,
+        IServiceCollection services) where T : class =>
+        @this
+            .GetConfig<T>()
+            .OnSuccess(services.AddSingleton)
+            .Map(@this);
+
+    public static Result<IConfiguration> AddConfig<T>(
+        this Result<IConfiguration> @this,
+        IServiceCollection services) where T : class => @this
+        .OnSuccess(configuration => configuration.AddConfig<T>(services));
+}
