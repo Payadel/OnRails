9d8e97e902ddc0cd1853e3b2a763cf676937b30d
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-17 02:28:21 +0330

    Adds OnFailExtensions.NewDetail

---
+++ OnRail/Extensions/OnFail/OnFailExtensions.NewDetailAync.cs
@@ -0,0 +1,58 @@
+using OnRail.Extensions.OnSuccess;
+using OnRail.Extensions.Try;
+using OnRail.ResultDetails;
+using OnRail.Extensions.Fail;
+
+namespace OnRail.Extensions.OnFail;
+
+public static partial class OnFailExtensions {
+    public static async Task<Result<T>> OnFail<T>(
+        this Task<Result<T>> source,
+        ErrorDetail? newErrorDetail,
+        int numOfTry = 1) {
+        var result = await TryExtensions.Try(source, numOfTry);
+        return result.IsSuccess ? result : result.Fail(newErrorDetail);
+    }
+
+    public static async Task<Result<T>> OnFail<T>(
+        this Result<T> source,
+        Func<Task<ErrorDetail?>> errorDetailFunc,
+        int numOfTry = 1) {
+        if (source.IsSuccess)
+            return source;
+
+        return await TryExtensions.Try(errorDetailFunc, numOfTry)
+            .OnSuccess(Result<T>.Fail);
+    }
+
+    public static async Task<Result> OnFail(
+        this Task<Result> source,
+        ErrorDetail? newErrorDetail,
+        int numOfTry = 1) {
+        var result = await TryExtensions.Try(source, numOfTry);
+        return result.IsSuccess ? result : Result.Fail(newErrorDetail);
+    }
+
+    public static async Task<Result> OnFail(
+        this Task<Result> source,
+        Func<ErrorDetail?> errorDetailFunc,
+        int numOfTry = 1) {
+        var result = await TryExtensions.Try(source, numOfTry);
+        if (result.IsSuccess)
+            return result;
+
+        return TryExtensions.Try(errorDetailFunc, numOfTry)
+            .OnSuccess(Result.Fail);
+    }
+
+    public static async Task<Result> OnFail(
+        this Result source,
+        Func<Task<ErrorDetail?>> errorDetailFunc,
+        int numOfTry = 1) {
+        if (source.IsSuccess)
+            return source;
+
+        return await TryExtensions.Try(errorDetailFunc, numOfTry)
+            .OnSuccess(Result.Fail);
+    }
+}
--- OnRail/Extensions/OnFail/OnFailExtensions.cs
+++ OnRail/Extensions/OnFail/OnFailExtensions.cs
@@ -1,54 +1,10 @@
 using OnRail.Extensions.Try;
-using OnRail.ResultDetails;
 
 namespace OnRail.Extensions.OnFail;
 
 //TODO: Test
 
 public static partial class OnFailExtensions {
-    public static Result<T> OnFail<T>(
-        this Result<T> source,
-        ErrorDetail? errorDetail
-    ) => source.IsSuccess ? source : source.Fail(errorDetail);
-
-    public static Result<T> OnFail<T>(
-        this Result<T> source,
-        Func<ErrorDetail?> errorDetailFunc,
-        int numOfTry = 1
-    ) => source.IsSuccess
-        ? source
-        : TryExtensions.Try(() => {
-            var errorDetail = errorDetailFunc();
-            return source.Fail(errorDetail);
-        }, numOfTry);
-
-    public static Result OnFail(
-        this Result source,
-        ErrorDetail? errorDetail
-    ) => source.IsSuccess ? source : source.Fail(errorDetail);
-
-    public static Result OnFail(
-        this Result source,
-        Func<ErrorDetail?> errorDetailFunc,
-        int numOfTry = 1
-    ) => source.IsSuccess
-        ? source
-        : TryExtensions.Try(() => {
-            var errorDetail = errorDetailFunc();
-            return source.Fail(errorDetail);
-        }, numOfTry);
-
-    public static Result<T> OnFail<T>(
-        this Result<T> source,
-        Func<Result<T>, ErrorDetail?> moreDetailFunc,
-        int numOfTry = 1
-    ) => source.IsSuccess
-        ? source
-        : TryExtensions.Try(() => {
-            var errorDetail = moreDetailFunc(source);
-            return source.Fail(errorDetail);
-        }, numOfTry);
-
     public static Result<T> OnFail<T>(
         this Result<T> source,
         Func<Result<T>, Result<T>> function,
@@ -77,19 +33,4 @@ public static partial class OnFailExtensions {
         int numOfTry = 1) => source.IsSuccess
         ? source
         : TryExtensions.Try(function, numOfTry);
-
-    public static Result OnFail(
-        this Result source,
-        Action action,
-        int numOfTry = 1
-    ) => source.IsSuccess
-        ? source
-        : TryExtensions.Try(action, numOfTry);
-
-    public static Result OnFail(
-        this Result source,
-        Action<Result> action,
-        int numOfTry = 1) => source.IsSuccess
-        ? source
-        : source.Try(action, numOfTry);
 }
