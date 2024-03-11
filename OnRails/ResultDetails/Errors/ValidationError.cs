596ca49ef71fae2e6bbc7432d8917afe16ec0a59
HamidMolareza <molareza@protonmail.com>
2024-03-11 13:37:05 +0330

    fix(ValidationError): make class `sealed`
    the `ValidationError` has `AddError` that returns `ValidationError`, so it is not suitable for inheritance
    
    BREAKING CHANGE: In this version the `ValidationError` is `sealed`. So you can not inheritance from it. Create your custom class from `ErrorDetail`.

--- OnRails/ResultDetails/Errors/ValidationError.cs
+++ OnRails/ResultDetails/Errors/ValidationError.cs
@@ -1,18 +1,13 @@
-using System.Text;
-
 namespace OnRails.ResultDetails.Errors;
 
-public class ValidationError(
+public sealed class ValidationError(
     string title = nameof(ValidationError),
     string? message = "One or more validation errors occurred.",
     int? statusCode = 400,
     Exception? exception = null,
     object? moreDetails = null)
     : ErrorDetail(title, message, statusCode, exception, moreDetails) {
-    public List<KeyValuePair<string, string>>? Errors { get; set; }
-
     public ValidationError AddError(string parameterName, string description) {
-        Errors ??= [];
         Errors.Add(new KeyValuePair<string, string>(parameterName, description));
         return this;
     }
@@ -22,15 +17,4 @@ public class ValidationError(
         Message,
         Errors
     };
-
-    public override string ToString() {
-        var sb = new StringBuilder(base.ToString());
-        if (Errors is not null) {
-            sb.AppendLine("Errors:\n");
-            foreach (var (key, value) in Errors)
-                sb.AppendLine($"\t{key}:\t{value}");
-        }
-
-        return sb.ToString();
-    }
 }
