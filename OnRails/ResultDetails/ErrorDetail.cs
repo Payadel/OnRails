3ea85a6b3784df16ab1e0c3a5f024d2320e3f0f1
HamidMolareza <molareza@protonmail.com>
2024-03-11 13:32:55 +0330

    feat(ErrorDetail): add key-value error list to `ErrorDetail`

--- OnRails/ResultDetails/ErrorDetail.cs
+++ OnRails/ResultDetails/ErrorDetail.cs
@@ -10,11 +10,18 @@ public class ErrorDetail(
     Exception? exception = null,
     object? moreDetails = null)
     : ResultDetail(title, message, statusCode, moreDetails) {
+    public List<KeyValuePair<string, string>> Errors { get; } = [];
     public StackTrace StackTrace { get; } = new(1, true);
     public Exception? Exception { get; } = exception;
 
     public override string ToString() {
         var sb = new StringBuilder(base.ToString());
+
+        if (Errors.Count > 0) {
+            sb.AppendLine("Errors:\n");
+            foreach (var (key, value) in Errors)
+                sb.AppendLine($"\t{key}:\t{value}");
+        }
 
         if (Exception is not null)
             sb.AppendLine($"Exception:\n\t{Exception}");
