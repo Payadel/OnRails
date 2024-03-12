c04a5e88e472c3791ae2fb8d4c4e996bb5279a68
HamidMolareza <molareza@protonmail.com>
2024-03-12 12:56:33 +0330

    feat(ConflictError): add objectIdentity filed
    We can specify which item has conflict.

--- OnRails/ResultDetails/Errors/ConflictError.cs
+++ OnRails/ResultDetails/Errors/ConflictError.cs
@@ -4,6 +4,15 @@ public class ConflictError(
     string title = nameof(ConflictError),
     string? message =
         "The request could not be completed due to a conflict with the current state of the target resource.",
+    string? objectIdentity = null,
     Exception? exception = null,
     object? moreDetails = null)
-    : ErrorDetail(title, message, 409, exception, moreDetails);
+    : ErrorDetail(title, message, 409, exception, moreDetails) {
+    public string? ObjectIdentity { get; } = objectIdentity;
+
+    protected override string CustomFieldsToString() {
+        return ObjectIdentity is null
+            ? ""
+            : $"Object Identity: {ObjectIdentity}";
+    }
+}
