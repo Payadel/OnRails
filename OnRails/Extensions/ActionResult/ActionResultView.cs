bc08f7e021157c8d0b2708c1048dcfdba796c009
HamidMolareza <molareza@protonmail.com>
2024-03-13 21:04:46 +0330

    fix(ActionResultView): returns 204 code for null value
    BREAKING CHANGE: In the previous version, 200 code was default for success result even we have not any value. But in this version we use 204 for null value.

--- OnRails/Extensions/ActionResult/ActionResultView.cs
+++ OnRails/Extensions/ActionResult/ActionResultView.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace OnRails.Extensions.ActionResult;
@@ -6,11 +7,23 @@ namespace OnRails.Extensions.ActionResult;
 [DebuggerStepThrough]
 public class ActionResultView : ObjectResult {
     public ActionResultView(ResultBase result) : base(result) {
-        var hasDetailView = result.Detail != null;
+        var hasDetailView = result.Detail is not null;
         var view = hasDetailView && result.Detail!.View;
 
-        StatusCode = view ? result.GetStatusCodeOrDefault() : result.Success ? 200 : 500;
-        Value = view ? result.Detail!.GetViewModel() : null;
+        if (view) {
+            // We have a detail data with view access
+            Value = result.Detail!.GetViewModel();
+            StatusCode = result.GetStatusCodeOrDefault(
+                StatusCodes.Status204NoContent,
+                StatusCodes.Status500InternalServerError);
+        }
+        else {
+            // We have not detail data or have not access to show
+            Value = null;
+            StatusCode = result.Success
+                ? StatusCodes.Status204NoContent
+                : StatusCodes.Status500InternalServerError;
+        }
     }
 }
 
@@ -19,11 +32,23 @@ public class ActionResultView<T> : ObjectResult {
     public ActionResultView(Result<T> result) : base(result) {
         var hasDetailView = result.Detail != null;
         var view = hasDetailView && result.Detail!.View;
+        var hasValue = result.Value is not null;
 
-        StatusCode = view ? result.GetStatusCodeOrDefault() : result.Success ? 200 : 500;
-
-        Value = result.Success ? result.Value : null;
-        if (Value is null && view)
-            Value = result.Detail!.GetViewModel();
+        if (view) {
+            // We have a detail data with view access
+            Value = result.Success ? result.Value : result.Detail!.GetViewModel();
+            StatusCode = result.GetStatusCodeOrDefault(
+                hasValue ? StatusCodes.Status200OK : StatusCodes.Status204NoContent,
+                StatusCodes.Status500InternalServerError);
+        }
+        else {
+            // We have not detail data or have not access to show
+            Value = result.Success ? result.Value : null;
+        
+            if (!result.Success)
+                StatusCode = 500;
+            else 
+                StatusCode = hasValue ? 200 : 204;
+        }
     }
 }
