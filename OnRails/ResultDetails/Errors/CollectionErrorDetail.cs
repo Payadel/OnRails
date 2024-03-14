5f5c253eb103f126b2a3a8538bd2b2bf7fc8572b
HamidMolareza <molareza@protonmail.com>
2024-03-14 11:33:20 +0330

    feat(ResultDetail): add CollectionErrorDetail.cs

---
+++ OnRails/ResultDetails/Errors/CollectionErrorDetail.cs
@@ -0,0 +1,39 @@
+using System.Text;
+
+namespace OnRails.ResultDetails.Errors;
+
+public class CollectionErrorDetail : ErrorDetail<ErrorDetail> {
+    public CollectionErrorDetail(
+        ErrorDetail error,
+        string title = nameof(CollectionErrorDetail),
+        string? message = "One or more error(s) occurred",
+        int? statusCode = 500,
+        object? moreDetails = null,
+        bool view = false) : base(error, title, message, statusCode,
+        moreDetails, view) { }
+
+    public CollectionErrorDetail(List<ErrorDetail> errors,
+        string title = nameof(CollectionErrorDetail),
+        string? message = "One or more error(s) occurred",
+        int? statusCode = 500,
+        object? moreDetails = null,
+        bool view = false) : base(
+        errors, title, message, statusCode, moreDetails, view) { }
+
+    protected override string? ErrorsToString() {
+        if (Errors.Count <= 0) return null;
+
+        var sb = new StringBuilder();
+
+        sb.AppendLine($"Errors ({Errors.Count}):");
+        for (var i = 0; i < Errors.Count; i++)
+            sb.AppendLine($"\t{i}- {Errors[i].ToString()}");
+
+        return sb.ToString();
+    }
+
+    public override bool IsTypeOf(Type type) => GetType() == type || Errors.Any(error => error.IsTypeOf(type));
+
+    public override bool IsTypeOf<TType>() where TType : class =>
+        this is TType || Errors.Any(error => error.IsTypeOf<TType>());
+}
