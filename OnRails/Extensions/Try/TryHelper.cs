0932cbe926b2bb82e90d393089ddcd6bee329cd6
HamidMolareza <molareza@protonmail.com>
2024-03-14 11:33:56 +0330

    fix(Try): use `CollectionErrorDetail` for save errors

--- OnRails/Extensions/Try/TryHelper.cs
+++ OnRails/Extensions/Try/TryHelper.cs
@@ -1,19 +1,19 @@
 using System.Text;
 using OnRails.ResultDetails;
+using OnRails.ResultDetails.Errors;
 
 namespace OnRails.Extensions.Try;
 
 internal static class TryHelper {
-    public static ErrorDetail GenerateError(List<ErrorDetail> errors, int numOfTry) {
+    public static CollectionErrorDetail GenerateError(List<ErrorDetail> errors, int numOfTry) {
         var message = new StringBuilder($"Operation failed with {numOfTry} attempts. ");
+
         if (errors.Count > 0) {
             message.Append(
-                $"The details of the {errors.Count} error(s) are stored in the '{nameof(ErrorDetail.Errors)}' field. ");
+                $"The details of the {errors.Count} error(s) are stored in the '{nameof(CollectionErrorDetail)}.{nameof(CollectionErrorDetail.Errors)}' field. ");
         }
 
-        var errorDetail = new ErrorDetail(message: message.ToString());
-        errorDetail.Errors.AddRange(errors);
-
+        var errorDetail = new CollectionErrorDetail(errors, message: message.ToString());
         return errorDetail;
     }
 }
