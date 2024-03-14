a77c30e63efad24ad40307a4c6e42d26b303cc43
HamidMolareza <molareza@protonmail.com>
2024-03-14 13:09:45 +0330

    fix(NotFoundError): use sing id instead of error list for saving items. Also make it optional.

--- OnRails/ResultDetails/Errors/NotFoundError.cs
+++ OnRails/ResultDetails/Errors/NotFoundError.cs
@@ -1,17 +1,25 @@
 ﻿namespace OnRails.ResultDetails.Errors;
 
-public class NotFoundError : ErrorDetail<object> {
-    public NotFoundError(
-        object itemId,
-        string title = nameof(NotFoundError),
-        string? message = "The requested resource(s) could not be found.",
-        object? moreDetails = null,
-        bool view = false) : base(itemId, title, message, 404, moreDetails, view) { }
-
-    public NotFoundError(
-        List<object> itemIds,
-        string title = nameof(NotFoundError),
-        string? message = "The requested resource(s) could not be found.",
-        object? moreDetails = null,
-        bool view = false) : base(itemIds, title, message, 404, moreDetails, view) { }
+public class NotFoundError(
+    object? id,
+    string title = nameof(NotFoundError),
+    string? message = "The requested resource(s) could not be found.",
+    object? moreDetails = null,
+    bool view = false)
+    : ErrorDetail(title, message, 404, moreDetails, view) {
+    public object? Id { get; } = id;
+    
+    public override Dictionary<string, object?> GetViewModel() =>
+        new() {
+            { nameof(Title), Title },
+            { nameof(Message), Message },
+            { nameof(Id), Id },
+        };
+    
+    public override bool IsTypeOf(Type type) => GetType() == type && Id is not null && Id.GetType() == type;
+    public override bool IsTypeOf<T>() where T : class => this is T  && Id is T;
+    
+    protected override string CustomFieldsToString() {
+        return $"{nameof(Id)}: {Id}";
+    }
 }
