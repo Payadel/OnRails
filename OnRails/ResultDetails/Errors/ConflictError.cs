e51d89c2fb5a039533b038787823592af6baa872
HamidMolareza <molareza@protonmail.com>
2024-03-14 11:51:13 +0330

    fix(ConflictError): make `errors` optional.
    For example, conflict may have happened due to duplicate Id.

--- OnRails/ResultDetails/Errors/ConflictError.cs
+++ OnRails/ResultDetails/Errors/ConflictError.cs
@@ -1,22 +1,13 @@
 ﻿namespace OnRails.ResultDetails.Errors;
 
-public class ConflictError : ErrorDetail<ConflictItem> {
-    public ConflictError(
-        ConflictItem item,
-        string title = nameof(ConflictError),
-        string? message =
-            "The request could not be completed due to a conflict with the current state of the target resource.",
-        object? moreDetails = null,
-        bool view = false) : base(item, title, message, 409, moreDetails, view) { }
-
-    public ConflictError(
-        List<ConflictItem> items,
-        string title = nameof(ConflictError),
-        string? message =
-            "The request could not be completed due to a conflict with the current state of the target resource.",
-        object? moreDetails = null,
-        bool view = false) : base(items, title, message, 409, moreDetails, view) { }
-
+public class ConflictError(
+    List<ConflictItem>? errors = null,
+    string title = nameof(ConflictError),
+    string? message =
+        "The request could not be completed due to a conflict with the current state of the target resource.",
+    object? moreDetails = null,
+    bool view = false)
+    : ErrorDetail<ConflictItem>(errors ?? [], title, message, 409, moreDetails, view) {
     public override bool IsTypeOf(Type type) => GetType() == type;
     public override bool IsTypeOf<TType>() where TType : class => this is TType;
 }
