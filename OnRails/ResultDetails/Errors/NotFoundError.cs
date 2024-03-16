ac1f3ba311d57a832847a99606050af46ab27f1c
HamidMolareza <molareza@protonmail.com>
2024-03-16 19:36:53 +0330

    feat(NotFoundError): use `List<KeyValue<object?>>` for save object keys

--- OnRails/ResultDetails/Errors/NotFoundError.cs
+++ OnRails/ResultDetails/Errors/NotFoundError.cs
@@ -1,25 +1,44 @@
-﻿namespace OnRails.ResultDetails.Errors;
+﻿using System.Text;
+using OnRails.Utilities;
 
-public class NotFoundError(
-    object? id,
-    string title = nameof(NotFoundError),
-    string? message = "The requested resource(s) could not be found.",
-    object? moreDetails = null,
-    bool view = false)
-    : ErrorDetail(title, message, 404, moreDetails, view) {
-    public object? Id { get; } = id;
-    
+namespace OnRails.ResultDetails.Errors;
+
+public class NotFoundError : ErrorDetail {
+    public NotFoundError(
+        string keyName,
+        object? keyValue,
+        string title = nameof(NotFoundError),
+        string? message = "The requested resource(s) could not be found.",
+        object? moreDetails = null,
+        bool view = false) : base(title, message, 404, moreDetails, view) {
+        Keys = [new(keyName, keyValue)];
+    }
+
+    public NotFoundError(
+        List<KeyValue<object?>> keys,
+        string title = nameof(NotFoundError),
+        string? message = "The requested resource(s) could not be found.",
+        object? moreDetails = null,
+        bool view = false) : base(title, message, 404, moreDetails, view) {
+        Keys = keys;
+    }
+
+    public List<KeyValue<object?>> Keys { get; }
+
     public override Dictionary<string, object?> GetViewModel() =>
         new() {
             { nameof(Title), Title },
             { nameof(Message), Message },
-            { nameof(Id), Id },
+            { nameof(Keys), Keys.ToDictionary() },
         };
-    
-    public override bool IsTypeOf(Type type) => GetType() == type && Id is not null && Id.GetType() == type;
-    public override bool IsTypeOf<T>() where T : class => this is T  && Id is T;
-    
+
     protected override string CustomFieldsToString() {
-        return $"{nameof(Id)}: {Id}";
+        var sb = new StringBuilder();
+
+        sb.AppendLine($"{nameof(Keys)}:");
+        foreach (var key in Keys)
+            sb.AppendLine($"\t{key.ToString()}");
+
+        return sb.ToString();
     }
 }
