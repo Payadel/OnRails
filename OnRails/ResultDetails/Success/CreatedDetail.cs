8a64c9281c34abd25aca84bec25c9a5e7c02d4c9
HamidMolareza <molareza@protonmail.com>
2024-03-17 01:45:45 +0330

    fix(CreatedDetail)!: big changes. add some props, update constructor, etc

--- OnRails/ResultDetails/Success/CreatedDetail.cs
+++ OnRails/ResultDetails/Success/CreatedDetail.cs
@@ -1,8 +1,50 @@
-﻿namespace OnRails.ResultDetails.Success;
+﻿using Microsoft.AspNetCore.Http;
 
-public class CreatedDetail(
-    string title = nameof(CreatedDetail),
-    string? message = "Resource created successfully.",
-    object? moreDetails = null,
-    bool view = false)
-    : SuccessDetail(title, message, 201, moreDetails, view);
+namespace OnRails.ResultDetails.Success;
+
+public class CreatedDetail : SuccessDetail {
+    public string? Location { get; }
+    public Uri? LocationUri { get; }
+    public string? ActionName { get; }
+    public string? ControllerName { get; }
+    public string? RouteName { get; }
+    public object? RouteValues { get; }
+
+
+    private const string DefaultTitle = nameof(CreatedDetail);
+    private const int DefaultStatusCode = StatusCodes.Status201Created;
+    private const bool DefaultView = true;
+
+    public CreatedDetail() :
+        base(DefaultTitle, null, DefaultStatusCode, null, DefaultView) { }
+
+    public CreatedDetail(string location) :
+        base(DefaultTitle, null, DefaultStatusCode, null, DefaultView) {
+        Location = location;
+    }
+
+    public CreatedDetail(Uri location) :
+        base(DefaultTitle, null, DefaultStatusCode, null, DefaultView) {
+        LocationUri = location;
+    }
+
+    public CreatedDetail(
+        string routeName,
+        object routeValues
+    ) : base(DefaultTitle, null, DefaultStatusCode, null, DefaultView) {
+        RouteName = routeName;
+        RouteValues = routeValues;
+    }
+
+    public CreatedDetail(
+        string actionName,
+        string? controllerName,
+        object routeValues
+    ) : base(DefaultTitle, null, DefaultStatusCode, null, DefaultView) {
+        ActionName = actionName;
+        ControllerName = controllerName;
+        RouteValues = routeValues;
+    }
+
+    public override Dictionary<string, object?> GetViewModel() => new();
+}
