f45a974128266743aae119588730f55e12c56ff5
HamidMolareza <molareza@protonmail.com>
2024-03-16 19:37:43 +0330

    fix(BadRequestError): use `KeyValue<object?>` to save errors

--- OnRails/ResultDetails/Errors/BadRequestError.cs
+++ OnRails/ResultDetails/Errors/BadRequestError.cs
@@ -2,24 +2,24 @@
 
 namespace OnRails.ResultDetails.Errors;
 
-public class BadRequestError : ErrorDetail<KeyValue<string>> {
+public class BadRequestError : ErrorDetail<KeyValue<object?>> {
     public BadRequestError(
-        List<KeyValue<string>> errors,
+        string errorKey,
+        object? errorValue,
+        string title = nameof(BadRequestError),
+        string? message = "Bad request. Please check your request parameters.",
+        int? statusCode = 400,
+        object? moreDetails = null,
+        bool view = false) : base(new KeyValue<object?>(errorKey, errorValue), title, message, statusCode, moreDetails,
+        view) { }
+
+    public BadRequestError(
+        List<KeyValue<object?>> errors,
         string title = nameof(BadRequestError),
         string? message = "Bad request. Please check your request parameters.",
         int? statusCode = 400,
         object? moreDetails = null,
         bool view = false) : base(errors, title, message, statusCode, moreDetails, view) { }
-
-    public BadRequestError(
-        string errorKey,
-        string errorValue,
-        string title = nameof(BadRequestError),
-        string? message = "Bad request. Please check your request parameters.",
-        int? statusCode = 400,
-        object? moreDetails = null,
-        bool view = false) : base(new KeyValue<string>(errorKey, errorValue), title, message, statusCode, moreDetails,
-        view) { }
 
 
     public override bool IsTypeOf(Type type) => GetType() == type;
--- OnRails/ResultDetails/Errors/ValidationError.cs
+++ OnRails/ResultDetails/Errors/ValidationError.cs
@@ -4,20 +4,20 @@ namespace OnRails.ResultDetails.Errors;
 
 public class ValidationError : BadRequestError {
     public ValidationError(
-        List<KeyValue<string>> errors,
-        string title = nameof(ValidationError),
-        string? message = "One or more validation errors occurred.",
-        int? statusCode = 400,
-        object? moreDetails = null,
-        bool view = false) : base(errors, title, message, statusCode, moreDetails, view) { }
-
-    public ValidationError(
         string errorKey,
-        string errorValue,
+        object? errorValue,
         string title = nameof(ValidationError),
         string? message = "One or more validation errors occurred.",
         int? statusCode = 400,
         object? moreDetails = null,
         bool view = false) : base(errorKey, errorValue, title, message, statusCode, moreDetails,
         view) { }
+
+    public ValidationError(
+        List<KeyValue<object?>> errors,
+        string title = nameof(ValidationError),
+        string? message = "One or more validation errors occurred.",
+        int? statusCode = 400,
+        object? moreDetails = null,
+        bool view = false) : base(errors, title, message, statusCode, moreDetails, view) { }
 }
