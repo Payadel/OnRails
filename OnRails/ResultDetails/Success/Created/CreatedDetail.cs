97be240a8fa15104ef96ecc247a740ec91fa268e
HamidMolareza <molareza@protonmail.com>
2024-03-17 15:33:48 +0330

    feat(CreatedDetail): break it to multi classes

---
+++ OnRails/ResultDetails/Success/Created/CreatedDetail.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+
+namespace OnRails.ResultDetails.Success.Created;
+
+[DebuggerStepThrough]
+public class CreatedDetail(
+    string? title = null,
+    string? message = null,
+    object? moreDetail = null,
+    bool? view = null)
+    : SuccessDetail(title ?? nameof(CreatedDetail), message, StatusCodes.Status201Created, moreDetail, view ?? true) {
+    public override Dictionary<string, object?> GetViewModel() => new();
+}
