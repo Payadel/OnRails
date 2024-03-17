475c7c1bbc9dad74d264daea309484ebbbc87ce1
HamidMolareza <molareza@protonmail.com>
2024-03-17 17:50:01 +0330

    feat(AcceptedAtActionDetail): add more constructor

--- OnRails/ResultDetails/Success/Accepted/AcceptedAtActionDetail.cs
+++ OnRails/ResultDetails/Success/Accepted/AcceptedAtActionDetail.cs
@@ -4,14 +4,37 @@ using System.Text;
 namespace OnRails.ResultDetails.Success.Accepted;
 
 [DebuggerStepThrough]
-public class AcceptedAtActionDetail(
-    string actionName,
-    string? controllerName,
-    object? routeValues)
-    : AcceptedDetail(nameof(AcceptedAtActionDetail)) {
-    public string ActionName { get; } = actionName;
-    public string? ControllerName { get; } = controllerName;
-    public object? RouteValues { get; } = routeValues;
+public class AcceptedAtActionDetail : AcceptedDetail {
+    public AcceptedAtActionDetail(string actionName) : base(nameof(AcceptedAtActionDetail)) {
+        ActionName = actionName;
+    }
+
+    public AcceptedAtActionDetail(
+        string actionName,
+        string controllerName) : base(nameof(AcceptedAtActionDetail)) {
+        ActionName = actionName;
+        ControllerName = controllerName;
+    }
+
+    public AcceptedAtActionDetail(
+        string actionName,
+        string controllerName,
+        object routeValues) : base(nameof(AcceptedAtActionDetail)) {
+        ActionName = actionName;
+        ControllerName = controllerName;
+        RouteValues = routeValues;
+    }
+
+    public AcceptedAtActionDetail(
+        string actionName,
+        object routeValues) : base(nameof(AcceptedAtActionDetail)) {
+        ActionName = actionName;
+        RouteValues = routeValues;
+    }
+
+    public string ActionName { get; }
+    public string? ControllerName { get; }
+    public object? RouteValues { get; }
 
     protected override string CustomFieldsToString() {
         var sb = new StringBuilder();
