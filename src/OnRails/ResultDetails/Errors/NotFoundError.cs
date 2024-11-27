95715acb7d07abdecbc5ea7ed30fab6eb00c8f1d
HamidMolareza <molareza@protonmail.com>
2024-11-27 22:17:32 +0330

    fix(NotFoundError): use `HashSet<string>` to store `Keys`

--- src/OnRails/ResultDetails/Errors/NotFoundError.cs
+++ src/OnRails/ResultDetails/Errors/NotFoundError.cs
@@ -1,48 +1,38 @@
 ﻿using System.Diagnostics;
-using System.Text;
 using Microsoft.AspNetCore.Http;
-using OnRails.Models;
-using OnRails.Utilities;
 
 namespace OnRails.ResultDetails.Errors;
 
 [DebuggerStepThrough]
 public class NotFoundError : ErrorDetail {
-    public NotFoundError(
-        string keyName,
-        object? keyValue,
-        string title = nameof(NotFoundError),
-        string? message = "The requested resource(s) could not be found.",
-        object? moreDetails = null,
-        bool view = false) : base(title, message, StatusCodes.Status404NotFound, moreDetails, view) {
-        Keys = [new(keyName, keyValue)];
+    private const string DefaultTitle = nameof(NotFoundError);
+    private const string DefaultMessage = "The requested resource(s) could not be found.";
+    private const int DefaultStatusCode = StatusCodes.Status404NotFound;
+
+    public NotFoundError(string key, bool view = false) : base(DefaultTitle, DefaultMessage, DefaultStatusCode, null,
+        view) {
+        Keys = [key];
     }
 
-    public NotFoundError(
-        List<KeyValue<object?>> keys,
-        string title = nameof(NotFoundError),
-        string? message = "The requested resource(s) could not be found.",
+    public NotFoundError(string title = DefaultTitle,
+        string? message = DefaultMessage,
+        HashSet<string>? keys = null,
         object? moreDetails = null,
         bool view = false) : base(title, message, StatusCodes.Status404NotFound, moreDetails, view) {
-        Keys = keys;
+        Keys = keys ?? [];
     }
 
-    public List<KeyValue<object?>> Keys { get; }
+    public HashSet<string> Keys { get; }
 
     public override Dictionary<string, object?> GetViewModel() =>
         new() {
             { nameof(Title), Title },
             { nameof(Message), Message },
-            { nameof(Keys), Keys.ToDictionary() },
+            { nameof(Keys), Keys.ToList() }
         };
 
-    protected override string CustomFieldsToString() {
-        var sb = new StringBuilder();
-
-        sb.AppendLine($"{nameof(Keys)}:");
-        foreach (var key in Keys)
-            sb.AppendLine($"\t{key.ToString()}");
-
-        return sb.ToString();
-    }
+    protected override string CustomFieldsToString() =>
+        Keys.Count == 0
+            ? string.Empty
+            : $"{nameof(Keys)}:\n{string.Join("\n\t", Keys)}";
 }
