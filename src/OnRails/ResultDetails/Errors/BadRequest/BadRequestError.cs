afbaad135565250b175940ca3b08714850a5a8a2
HamidMolareza <molareza@protonmail.com>
2024-11-27 22:25:21 +0330

    fix(BadRequestError)!: use `Dictionary<string, List<string>> Errors` as custom field

--- src/OnRails/ResultDetails/Errors/BadRequest/BadRequestError.cs
+++ src/OnRails/ResultDetails/Errors/BadRequest/BadRequestError.cs
@@ -1,38 +1,60 @@
 ﻿using System.Diagnostics;
+using System.Text;
 using Microsoft.AspNetCore.Http;
 using OnRails.Models;
-using OnRails.Utilities;
 
 namespace OnRails.ResultDetails.Errors.BadRequest;
 
 [DebuggerStepThrough]
-public class BadRequestError : ErrorDetail<KeyValue<object?>> {
-    public BadRequestError(
-        string errorKey,
-        object? errorValue,
-        string title = nameof(BadRequestError),
-        string? message = "Bad request. Please check your request parameters.",
-        int? statusCode = StatusCodes.Status400BadRequest,
-        object? moreDetails = null,
-        bool view = false) : base(new KeyValue<object?>(errorKey, errorValue), title, message, statusCode, moreDetails,
-        view) { }
+public class BadRequestError : ErrorDetail {
+    private const string DefaultTitle = nameof(BadRequestError);
+    private const string DefaultMessage = "Bad request. Please check your request parameters.";
+    private const int DefaultStatusCode = StatusCodes.Status400BadRequest;
+
+    public Dictionary<string, List<string>> Errors { get; protected init; }
 
     public BadRequestError(
-        List<KeyValue<object?>> errors,
-        string title = nameof(BadRequestError),
-        string? message = "Bad request. Please check your request parameters.",
-        int? statusCode = StatusCodes.Status400BadRequest,
+        string fieldName,
+        string fieldMessage,
+        bool view = false) : base(DefaultTitle, DefaultMessage, DefaultStatusCode, null, view) {
+        Errors = [];
+        Errors.Add(fieldName, [fieldMessage]);
+    }
+
+    public BadRequestError(
+        Dictionary<string, List<string>>? errors = null,
+        string title = DefaultTitle,
+        string? message = DefaultMessage,
+        int? statusCode = DefaultStatusCode,
         object? moreDetails = null,
-        bool view = false) : base(errors, title, message, statusCode, moreDetails, view) { }
-
-
-    public override bool IsTypeOf(Type type) => GetType() == type;
-    public override bool IsTypeOf<TType>() where TType : class => this is TType;
+        bool view = false) : base(title, message, statusCode, moreDetails, view) {
+        Errors = errors ?? [];
+    }
 
     public override Dictionary<string, object?> GetViewModel() =>
         new() {
             { nameof(Title), Title },
-            { nameof(Message), Message },
-            { nameof(Errors), Errors.ToDictionary() }
+            { nameof(Message), Message }, {
+                nameof(Errors), Errors.Select(error =>
+                    new FieldErrors(error.Key, error.Value)).ToList()
+            }
         };
+
+    protected override string CustomFieldsToString() {
+        if (Errors.Count == 0) return string.Empty;
+
+        var sb = new StringBuilder();
+        sb.AppendLine("Errors: ");
+        foreach (var (fieldName, message) in Errors) {
+            sb.Append($"- {fieldName}: ");
+            if (message.Count <= 1) {
+                sb.AppendLine(message.FirstOrDefault() ?? string.Empty);
+            }
+            else {
+                sb.AppendLine($"\n\t{string.Join("\n\t", message)}");
+            }
+        }
+
+        return sb.ToString();
+    }
 }
