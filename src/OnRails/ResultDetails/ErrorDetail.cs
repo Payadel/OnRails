9b279faa146474f4d32509ff9ddc379a261cb165
HamidMolareza <molareza@protonmail.com>
2024-11-27 22:14:52 +0330

    fix(ErrorDetail): remove `ErrorDetail<T>`

--- src/OnRails/ResultDetails/ErrorDetail.cs
+++ src/OnRails/ResultDetails/ErrorDetail.cs
@@ -1,7 +1,7 @@
 using System.Diagnostics;
 using System.Text;
 using Microsoft.AspNetCore.Http;
-using OnRails.Utilities;
+using OnRails.Helpers;
 
 namespace OnRails.ResultDetails;
 
@@ -22,59 +22,4 @@ public class ErrorDetail(
         sb.AppendLine($"StackTrace:\n\t{StackTrace.ToString().Trim()}");
         return sb.ToString();
     }
-
-    public override Dictionary<string, object?> GetViewModel() =>
-        new() {
-            { nameof(Title), Title },
-            { nameof(Message), Message },
-        };
-}
-
-public abstract class ErrorDetail<T> : ErrorDetail where T : class {
-    protected ErrorDetail(
-        T error,
-        string title = nameof(ErrorDetail),
-        string? message = "An error occurred",
-        int? statusCode = StatusCodes.Status500InternalServerError,
-        object? moreDetails = null,
-        bool view = false) : base(title, message, statusCode, moreDetails, view) {
-        Errors ??= [];
-        Errors.Add(error);
-    }
-
-    protected ErrorDetail(
-        List<T> errors,
-        string title = nameof(ErrorDetail),
-        string? message = "An error occurred",
-        int? statusCode = StatusCodes.Status500InternalServerError,
-        object? moreDetails = null,
-        bool view = false) : base(title, message, statusCode, moreDetails, view) {
-        Errors = errors;
-    }
-
-    public List<T> Errors { get; }
-
-    protected override string? CustomFieldsToString() => ErrorsToString();
-
-    protected virtual string? ErrorsToString() {
-        if (Errors.Count <= 0) return null;
-
-        var sb = new StringBuilder();
-
-        sb.AppendLine($"Errors ({Errors.Count}):");
-        foreach (var error in Errors)
-            sb.AppendLine($"\t{error.ToString()}");
-
-        return sb.ToString();
-    }
-
-    public override Dictionary<string, object?> GetViewModel() =>
-        new() {
-            { nameof(Title), Title },
-            { nameof(Message), Message },
-            { nameof(Errors), Errors }
-        };
-
-    public override bool IsTypeOf(Type type) => GetType() == type || Errors.Any(error => error.GetType() == type);
-    public override bool IsTypeOf<TType>() where TType : class => this is TType || Errors.Any(error => error is TType);
 }
