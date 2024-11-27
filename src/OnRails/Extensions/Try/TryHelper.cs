9bf4e2a9ab662a354d6960de5476f66e1b006525
HamidMolareza <molareza@protonmail.com>
2024-11-27 22:27:04 +0330

    chore: minor updates

--- src/OnRails/Extensions/Try/TryHelper.cs
+++ src/OnRails/Extensions/Try/TryHelper.cs
@@ -5,15 +5,15 @@ using OnRails.ResultDetails.Errors.Internal;
 namespace OnRails.Extensions.Try;
 
 internal static class TryHelper {
-    public static CollectionErrorDetail GenerateError(List<ErrorDetail> errors, int numOfTry) {
+    public static ErrorDetailList GenerateError(List<ErrorDetail> errors, int numOfTry) {
         var message = new StringBuilder($"Operation failed with {numOfTry} attempts. ");
 
         if (errors.Count > 0) {
             message.Append(
-                $"The details of the {errors.Count} error(s) are stored in the '{nameof(CollectionErrorDetail)}.{nameof(CollectionErrorDetail.Errors)}' field. ");
+                $"The details of the {errors.Count} error(s) are stored in the '{nameof(ErrorDetailList)}.{nameof(ErrorDetailList.Errors)}' field. ");
         }
 
-        var errorDetail = new CollectionErrorDetail(errors, message: message.ToString());
+        var errorDetail = new ErrorDetailList(errors, message: message.ToString());
         return errorDetail;
     }
 }
