7aaf46010bb1a59667aa717aea56d38d187df867
HamidMolareza <molareza@protonmail.com>
2024-11-27 22:26:20 +0330

    fix(ValidationError): match with `BadRequestError` class

--- src/OnRails/ResultDetails/Errors/BadRequest/ValidationError.cs
+++ src/OnRails/ResultDetails/Errors/BadRequest/ValidationError.cs
@@ -1,26 +1,47 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Http;
-using OnRails.Models;
 
 namespace OnRails.ResultDetails.Errors.BadRequest;
 
 [DebuggerStepThrough]
 public class ValidationError : BadRequestError {
-    public ValidationError(
-        string errorKey,
-        object? errorValue,
-        string title = nameof(ValidationError),
-        string? message = "One or more validation errors occurred.",
-        int? statusCode = StatusCodes.Status400BadRequest,
-        object? moreDetails = null,
-        bool view = false) : base(errorKey, errorValue, title, message, statusCode, moreDetails,
-        view) { }
+    private const string DefaultTitle = nameof(ValidationError);
+    private const int DefaultStatusCode = StatusCodes.Status400BadRequest;
+
+    private const string DefaultMessage =
+        "One or more validation errors occurred.";
 
     public ValidationError(
-        List<KeyValue<object?>> errors,
-        string title = nameof(ValidationError),
-        string? message = "One or more validation errors occurred.",
-        int? statusCode = StatusCodes.Status400BadRequest,
+        string fieldName,
+        string fieldMessage,
+        bool view = false) : base(title: DefaultTitle, message: DefaultMessage, statusCode: DefaultStatusCode,
+        moreDetails: null, view: view) {
+        Errors.Add(fieldName, [fieldMessage]);
+    }
+
+    public ValidationError(Dictionary<string, string>? errors = null,
+        string title = DefaultTitle,
+        string? message = DefaultMessage,
+        int? statusCode = DefaultStatusCode,
         object? moreDetails = null,
-        bool view = false) : base(errors, title, message, statusCode, moreDetails, view) { }
+        bool view = false) : base(title: title, message: message, statusCode: statusCode,
+        moreDetails: moreDetails, view: view) {
+        if (errors is not null) {
+            Errors = errors
+                .ToDictionary(e => e.Key,
+                    e => new List<string> { e.Value });
+        }
+    }
+
+    public ValidationError(Dictionary<string, List<string>>? errors = null,
+        string title = DefaultTitle,
+        string? message = DefaultMessage,
+        int? statusCode = DefaultStatusCode,
+        object? moreDetails = null,
+        bool view = false) : base(title: title, message: message, statusCode: statusCode,
+        moreDetails: moreDetails, view: view) {
+        if (errors is not null) {
+            Errors = errors;
+        }
+    }
 }
