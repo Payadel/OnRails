604da2688f12687afe51c6bc6b61a66320086085
HamidMolareza <molareza@protonmail.com>
2024-11-28 23:40:59 +0330

    feat(TeeUsing): add overloads

--- src/OnRails/Extensions/Using/UsingExtensions.TeeUsing.cs
+++ src/OnRails/Extensions/Using/UsingExtensions.TeeUsing.cs
@@ -14,4 +14,64 @@ public static partial class UsingExtensions {
         Func<TResult> function,
         int numOfTry = 1) where TSource : IDisposable =>
         obj.Tee(() => obj.Using(function, numOfTry));
+
+    public static TSource TeeUsing<TSource, TResult>(
+        this TSource obj,
+        Func<Result<TResult>> function,
+        int numOfTry = 1) where TSource : IDisposable =>
+        obj.Tee(() => obj.Using(function, numOfTry));
+
+    public static TSource TeeUsing<TSource, TResult>(
+        this TSource obj,
+        Func<TSource, Result<TResult>> function,
+        int numOfTry = 1) where TSource : IDisposable =>
+        obj.Tee(() => obj.Using(function, numOfTry));
+
+    public static TSource TeeUsing<TSource>(
+        this TSource obj,
+        Func<Result> function,
+        int numOfTry = 1) where TSource : IDisposable =>
+        obj.Tee(() => obj.Using(function, numOfTry));
+
+    public static TSource TeeUsing<TSource>(
+        this TSource obj,
+        Func<TSource, Result> function,
+        int numOfTry = 1) where TSource : IDisposable =>
+        obj.Tee(() => obj.Using(function, numOfTry));
+
+    public static Task<TSource> TeeUsing<TSource, TResult>(
+        this Task<TSource> obj,
+        Func<Task<TResult>> function,
+        int numOfTry = 1) where TSource : IDisposable =>
+        obj.Tee(() => obj.Using(function, numOfTry));
+
+    public static Task<TSource> TeeUsing<TSource, TResult>(
+        this Task<TSource> obj,
+        Func<Task<Result<TResult>>> function,
+        int numOfTry = 1) where TSource : IDisposable =>
+        obj.Tee(() => obj.Using(function, numOfTry));
+
+    public static Task<TSource> TeeUsing<TSource, TResult>(
+        this Task<TSource> obj,
+        Func<TSource, Task<TResult>> function,
+        int numOfTry = 1) where TSource : IDisposable =>
+        obj.Tee(() => obj.Using(function, numOfTry));
+
+    public static Task<TSource> TeeUsing<TSource, TResult>(
+        this Task<TSource> obj,
+        Func<TSource, Task<Result<TResult>>> function,
+        int numOfTry = 1) where TSource : IDisposable =>
+        obj.Tee(() => obj.Using(function, numOfTry));
+
+    public static Task<TSource> TeeUsing<TSource>(
+        this Task<TSource> obj,
+        Func<Task<Result>> function,
+        int numOfTry = 1) where TSource : IDisposable =>
+        obj.Tee(() => obj.Using(function, numOfTry));
+
+    public static Task<TSource> TeeUsing<TSource>(
+        this Task<TSource> obj,
+        Func<TSource, Task<Result>> function,
+        int numOfTry = 1) where TSource : IDisposable =>
+        obj.Tee(() => obj.Using(function, numOfTry));
 }
